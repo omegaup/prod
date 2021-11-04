d4012b634d8565ce8def16289e91cbd485f74009
lhchavez <lhchavez@lhchavez.com>
2021-11-03 19:36:28 -0700

    Use Redis as cache backend

--- k8s/omegaup/overlays/development/omegaup-secret/config.php
+++ k8s/omegaup/overlays/development/omegaup-secret/config.php
@@ -1,15 +1,17 @@
 <?php
 define('OMEGAUP_ALLOW_PRIVILEGE_SELF_ASSIGNMENT', true);
+define('OMEGAUP_CACHE_IMPLEMENTATION', 'redis');
 define('OMEGAUP_CSP_LOG_FILE', '/tmp/csp.log');
-define('OMEGAUP_URL', 'http://omegaup.test');
 define('OMEGAUP_DB_HOST', 'mysql-service');
 define('OMEGAUP_DB_NAME', 'omegaup');
 define('OMEGAUP_DB_PASS', 'omegaup');
 define('OMEGAUP_DB_USER', 'omegaup');
-define('OMEGAUP_ENVIRONMENT', 'production');
-define('OMEGAUP_LOG_FILE', '/var/log/frontend.log');
 define('OMEGAUP_ENABLE_SOCIAL_MEDIA_RESOURCES', false);
-define('SMARTY_CACHE_DIR', '/tmp');
+define('OMEGAUP_ENVIRONMENT', 'production');
+define('OMEGAUP_GITSERVER_SECRET_TOKEN', 'secret');
 define('OMEGAUP_GITSERVER_URL', 'http://gitserver-service:33861');
 define('OMEGAUP_GRADER_URL', 'https://grader-service:21680');
-define('OMEGAUP_GITSERVER_SECRET_TOKEN', 'secret');
+define('OMEGAUP_LOG_FILE', '/var/log/frontend.log');
+define('OMEGAUP_URL', 'http://omegaup.test');
+define('REDIS_HOST', 'redis-service');
+define('SMARTY_CACHE_DIR', '/tmp');
