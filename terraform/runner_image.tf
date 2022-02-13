73f189857b4cffded552704c98092c2aec8eac89
lhchavez <lhchavez@lhchavez.com>
2022-02-12 19:24:47 -0800

    Uprev the runner to use omegajail v3.8.0

--- terraform/runner_image.tf
+++ terraform/runner_image.tf
@@ -25,12 +25,12 @@ resource "azurerm_shared_image" "runner" {
 }
 
 data "azurerm_image" "runner" {
-  name                = "omegaup-runner-image-20220108"
+  name                = "omegaup-runner-image-20220212"
   resource_group_name = azurerm_resource_group.runner_image_builder.name
 }
 
 resource "azurerm_shared_image_version" "runner" {
-  name                = "1.0.2"
+  name                = "1.0.3"
   gallery_name        = azurerm_shared_image_gallery.runner.name
   image_name          = azurerm_shared_image.runner.name
   resource_group_name = azurerm_resource_group.runner_image_builder.name
