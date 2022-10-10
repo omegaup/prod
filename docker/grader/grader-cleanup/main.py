02958473efc9fb344f828570c5d8335a8208249b
lhchavez <lhchavez@lhchavez.com>
2022-10-09 17:36:13 -0700

    Fix the `grader-cleanup` cronjob and make it actually delete files

--- docker/grader/grader-cleanup/main.py
+++ docker/grader/grader-cleanup/main.py
@@ -6,6 +6,7 @@ import itertools
 import logging
 import os
 import os.path
+import shutil
 import time
 
 from typing import Iterable, Tuple
@@ -18,20 +19,23 @@ def _walk(
     min_age_days: int,
 ) -> Iterable[Tuple[str, int]]:
     now = time.time()
+    root = os.path.realpath(root)
     for dirname, dirs, files in os.walk(root):
         rel = os.path.relpath(dirname, root)
         current_depth = 0
         if rel != '.':
             current_depth = 1 + sum(1 for c in rel if c == '/')
-        if current_depth == depth:
-            for name in (files if file else dirs):
-                path = os.path.join(dirname, name)
-                stat = os.stat(path)
-                age_in_days = int(now - stat.st_ctime) // 3600 // 24
-                if age_in_days >= min_age_days:
-                    yield (path, stat.st_ctime)
-            # Prevent recursion.
-            dirs.clear()
+        if current_depth < depth:
+            continue
+        for name in (files if file else dirs):
+            path = os.path.join(dirname, name)
+            stat = os.stat(path)
+            ctime = min(stat.st_mtime, stat.st_ctime)
+            age_in_days = int(now - ctime) // 3600 // 24
+            if age_in_days >= min_age_days:
+                yield (path, ctime)
+        # Prevent recursion.
+        dirs.clear()
 
 
 def _main() -> None:
@@ -41,20 +45,14 @@ def _main() -> None:
     for path, ctime in itertools.chain(
             _walk(
                 '/var/lib/omegaup/ephemeral',
-                depth=1,
+                depth=0,
                 file=False,
                 min_age_days=1,
             ),
             _walk(
                 '/var/lib/omegaup/grade',
-                depth=3,
+                depth=2,
                 file=False,
-                min_age_days=128,
-            ),
-            _walk(
-                '/var/lib/omegaup/submissions',
-                depth=1,
-                file=True,
                 min_age_days=128,
             ),
     ):
@@ -62,8 +60,20 @@ def _main() -> None:
             '%s %s', path,
             datetime.datetime.utcfromtimestamp(ctime).strftime(
                 '%Y-%m-%dT%H:%M:%SZ'))
+        shutil.rmtree(path)
+
+    for path, ctime in _walk(
+            '/var/lib/omegaup/submissions',
+            depth=1,
+            file=True,
+            min_age_days=128,
+    ):
+        logging.info(
+            '%s %s', path,
+            datetime.datetime.utcfromtimestamp(ctime).strftime(
+                '%Y-%m-%dT%H:%M:%SZ'))
+        os.remove(path)
     logging.info('done')
-    time.sleep(999999)
 
 
 if __name__ == '__main__':
