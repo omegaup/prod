0442bfcab16d988f6584485d86e52e3ea043dd5a
lhchavez <lhchavez@lhchavez.com>
2022-11-07 18:42:53 -0800

    Add the `omegaup-backup` bucket

--- terraform/grader.tf
+++ terraform/grader.tf
@@ -49,6 +49,27 @@ resource "kubernetes_persistent_volume" "omegaup_grader_pv" {
   }
 }
 
+resource "aws_s3_bucket" "omegaup_backup" {
+  bucket = "omegaup-backup"
+
+  tags = {
+  }
+}
+
+resource "aws_s3_bucket_acl" "omegaup_backup" {
+  bucket = aws_s3_bucket.omegaup_backup.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_public_access_block" "omegaup_backup" {
+  bucket = aws_s3_bucket.omegaup_backup.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
 resource "aws_s3_bucket" "omegaup_runs" {
   bucket = "omegaup-runs"
 
@@ -104,7 +125,7 @@ resource "aws_iam_policy" "grader" {
         ]
         Effect = "Allow"
         Resource = [
-          "arn:aws:s3:::omegaup-backup/omegaup/submissions/*",
+          "arn:aws:s3:::${aws_s3_bucket.omegaup_backup.bucket}/omegaup/submissions/*",
           "arn:aws:s3:::${aws_s3_bucket.omegaup_runs.bucket}/*",
           "arn:aws:s3:::${aws_s3_bucket.omegaup_submissions.bucket}/*",
         ]
