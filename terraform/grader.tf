5b61bc690cc4b803f84cd17ac69fee9c204a5911
lhchavez <lhchavez@lhchavez.com>
2022-11-10 21:00:22 -0800

    Delete the `omegaup-backup` bucket
    This bucket has served us well. But it's time to delete it because all
    of its contents are now in other buckets.

--- terraform/grader.tf
+++ terraform/grader.tf
@@ -49,27 +49,6 @@ resource "kubernetes_persistent_volume" "omegaup_grader_pv" {
   }
 }
 
-resource "aws_s3_bucket" "omegaup_backup" {
-  bucket = "omegaup-backup"
-
-  tags = {
-  }
-}
-
-resource "aws_s3_bucket_acl" "omegaup_backup" {
-  bucket = aws_s3_bucket.omegaup_backup.id
-  acl    = "private"
-}
-
-resource "aws_s3_bucket_public_access_block" "omegaup_backup" {
-  bucket = aws_s3_bucket.omegaup_backup.id
-
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
-
 resource "aws_s3_bucket" "omegaup_runs" {
   bucket = "omegaup-runs"
 
