fc24524696f30204633d88bf656068196553fafd
Luis Héctor Chávez <lhchavez@chromium.org>
2025-02-20 22:40:31 -0800

    Bump the runner image version

--- terraform/runner_image.tf
+++ terraform/runner_image.tf
@@ -11,7 +11,7 @@ resource "azurerm_shared_image_gallery" "runner" {
 }
 
 resource "azurerm_shared_image" "runner" {
-  name                = "omegaup-runner-image-20221002"
+  name                = "omegaup-runner-image-20250220"
   gallery_name        = azurerm_shared_image_gallery.runner.name
   resource_group_name = azurerm_resource_group.runner_image_builder.name
   location            = azurerm_resource_group.runner_image_builder.location
@@ -20,17 +20,17 @@ resource "azurerm_shared_image" "runner" {
   identifier {
     publisher = "omegaUp"
     offer     = "runner"
-    sku       = "20221002"
+    sku       = "20250220"
   }
 }
 
 data "azurerm_image" "runner" {
-  name                = "omegaup-runner-image-20221002"
+  name                = "omegaup-runner-image-20250220"
   resource_group_name = azurerm_resource_group.runner_image_builder.name
 }
 
 resource "azurerm_shared_image_version" "runner" {
-  name                = "1.0.7"
+  name                = "1.0.9"
   gallery_name        = azurerm_shared_image_gallery.runner.name
   image_name          = azurerm_shared_image.runner.name
   resource_group_name = azurerm_resource_group.runner_image_builder.name
